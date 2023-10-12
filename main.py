8214503cbdaedbb916063a6be1e3e5fc82228327
Shang Jian Deng <sjdeng@uwaterloo.ca>
2023-10-12 16:02:05 -0400

    input playlist from command line

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from spotify import get_playlist
 from youtube import get_video_id
 from youtube_to_mp3 import download_video
 
-# for track in playlist['items']:
-#     track_name = track['track']['name']
-#     artists = [artist['name'] for artist in track['track']['artists']]
-#     print(f"Track: {track_name}, Artists: {', '.join(artists)}")
-
 if __name__ == '__main__':
-    playlist = get_playlist('https://open.spotify.com/playlist/2B17v6HfMJPIgHcOhMYnSd?si=2008c80328734a4f')
+    playlist_uri = input("Enter the URI for a spotify playlist: ")
+    playlist = get_playlist(playlist_uri)
     for song, artists in playlist:
         print(f"Getting Video ID for Track: {song}, Artists: {', '.join(artists)}")
         video_id = get_video_id(song + ' ' + ' '.join(artists))
--- spotify.py
+++ spotify.py
@@ -14,11 +14,7 @@ client_secret = os.getenv("CLIENT_SECRET")
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# playlist_uri = 'https://open.spotify.com/playlist/2B17v6HfMJPIgHcOhMYnSd?si=2008c80328734a4f'
-# playlist = sp.playlist_tracks(playlist_uri)
-
 def get_playlist(playlist_uri):
-    playlist_uri = 'https://open.spotify.com/playlist/2B17v6HfMJPIgHcOhMYnSd?si=2008c80328734a4f'
     playlist = sp.playlist_tracks(playlist_uri)
 
     tracks = []
@@ -26,6 +22,5 @@ def get_playlist(playlist_uri):
         track_name = track['track']['name']
         artists = [artist['name'] for artist in track['track']['artists']]
         tracks.append((track_name, artists))
-        # print(f"Track: {track_name}, Artists: {', '.join(artists)}")
 
     return tracks
